0ca8b9f86dd0330a1cefc38b47f44a4a99979ba2
Carlos Sac <carlos.sac@outlook.com>
2023-11-27 04:20:45 -0500

    updated Dining Philosophers Problem

--- multithreading/DiningPhilosophers.cpp
+++ multithreading/DiningPhilosophers.cpp
@@ -1,36 +1,67 @@
 #include <iostream>
 #include <thread>
 #include <mutex>
+#include <condition_variable>
+#include <vector>
 using namespace std;
+
+
 
 const int N = 5; // number of philosophers
 mutex forks[N]; // mutexes for forks
 mutex cout_mutex; // mutex for cout
 bool stop = false; // flag to stop philosophers
+vector<int> eat_count(N, 0); // count of eating times for each philosopher
+vector<int> think_count(N, 0); // count of thinking times for each philosopher
+int semaphore = N - 1; // semaphore to control number of philosophers eating
+condition_variable cv; // condition variable for the semaphore
+mutex semaphore_mutex; // mutex for the semaphore
 
 void philosopher(int i) {
-    while (!stop) {
-        cout_mutex.lock();
-        cout << "Philosopher " << i << " is thinking.\n";
-        cout_mutex.unlock();
-        if (i == N - 1) { // the last philosopher picks up the right fork first
-            forks[0].lock();
-            forks[i].lock();
+    try {
+        while (!stop) {
+            cout_mutex.lock();
+
+            // cout << "Philosopher " << i << " is thinking.\n";
+
+            think_count[i]++; // increment thinking count
+            cout_mutex.unlock();
+
+            unique_lock<mutex> semaphore_lock(semaphore_mutex);
+            cv.wait(semaphore_lock, [&]() { return semaphore > 0; }); // wait for the semaphore
+            --semaphore; // decrement the semaphore
+            semaphore_lock.unlock();
+
+            // Determine the order of forks for each philosopher
+            int first_fork = min(i, (i+1)%N);
+            int second_fork = max(i, (i+1)%N);
+
+            unique_lock<mutex> lock1(forks[first_fork], defer_lock);
+            unique_lock<mutex> lock2(forks[second_fork], defer_lock);
+
+            lock(lock1, lock2); // lock both forks at once
+
+            cout_mutex.lock();
+
+            // cout << "Philosopher " << i << " is eating.\n";
+            
+            eat_count[i]++; // increment eating count
+            cout_mutex.unlock();
+
+            lock1.unlock(); // release the first fork
+            lock2.unlock(); // release the second fork
+
+            semaphore_lock.lock();
+            ++semaphore; // increment the semaphore
+            cv.notify_one(); // notify one waiting philosopher
         }
-        else { // the other philosophers pick up the left fork first
-            forks[i].lock();
-            forks[(i + 1) % N].lock();
-        }
-        cout_mutex.lock();
-        cout << "Philosopher " << i << " is eating.\n";
-        cout_mutex.unlock();
-        forks[i].unlock();
-        forks[(i + 1) % N].unlock();
+    } catch (const exception& e) {
+        cerr << "Exception in philosopher " << i << ": " << e.what() << '\n';
     }
 }
 
 void timer() {
-    this_thread::sleep_for(chrono::seconds(5));
+    this_thread::sleep_for(chrono::seconds(3));
     stop = true;
 }
 
@@ -47,5 +78,11 @@ int main() {
         philosophers[i].join();
     }
     t.join();
+
+    // print report
+    for (int i = 0; i < N; i++) {
+        cout << "Philosopher " << i << " ate " << eat_count[i] << " times and thought " << think_count[i] << " times.\n";
+    }
+
     return 0;
 }
